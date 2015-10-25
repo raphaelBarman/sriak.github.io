45258f0294af7dfdfc4c31d0783d50d4e05f678e
Raphael Barman <raphael.barman@epfl.ch>
2015-10-25 21:45:46 +0100

    up to week 6

---
+++ auto/compnet.el
@@ -0,0 +1,103 @@
+(TeX-add-style-hook
+ "compnet"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline23"
+    "sec:orgheadline14"
+    "sec:orgheadline1"
+    "sec:orgheadline6"
+    "sec:orgheadline2"
+    "sec:orgheadline3"
+    "sec:orgheadline4"
+    "sec:orgheadline5"
+    "sec:orgheadline7"
+    "sec:orgheadline8"
+    "sec:orgheadline9"
+    "sec:orgheadline10"
+    "sec:orgheadline11"
+    "sec:orgheadline12"
+    "sec:orgheadline13"
+    "sec:orgheadline22"
+    "sec:orgheadline17"
+    "sec:orgheadline15"
+    "sec:orgheadline16"
+    "sec:orgheadline18"
+    "sec:orgheadline19"
+    "sec:orgheadline20"
+    "sec:orgheadline21"
+    "sec:orgheadline52"
+    "sec:orgheadline26"
+    "sec:orgheadline24"
+    "sec:orgheadline25"
+    "sec:orgheadline29"
+    "sec:orgheadline27"
+    "sec:orgheadline28"
+    "sec:orgheadline30"
+    "sec:orgheadline31"
+    "sec:orgheadline32"
+    "sec:orgheadline42"
+    "sec:orgheadline33"
+    "sec:orgheadline34"
+    "sec:orgheadline35"
+    "sec:orgheadline38"
+    "sec:orgheadline36"
+    "sec:orgheadline37"
+    "sec:orgheadline39"
+    "sec:orgheadline40"
+    "sec:orgheadline41"
+    "sec:orgheadline48"
+    "sec:orgheadline43"
+    "sec:orgheadline47"
+    "sec:orgheadline44"
+    "sec:orgheadline45"
+    "sec:orgheadline46"
+    "sec:orgheadline51"
+    "sec:orgheadline49"
+    "sec:orgheadline50"
+    "sec:orgheadline66"
+    "sec:orgheadline55"
+    "sec:orgheadline53"
+    "sec:orgheadline54"
+    "sec:orgheadline65"
+    "sec:orgheadline56"
+    "sec:orgheadline57"
+    "sec:orgheadline58"
+    "sec:orgheadline59"
+    "sec:orgheadline60"
+    "sec:orgheadline61"
+    "sec:orgheadline64"
+    "sec:orgheadline62"
+    "sec:orgheadline63"
+    "sec:orgheadline67"
+    "sec:orgheadline68")))
+
